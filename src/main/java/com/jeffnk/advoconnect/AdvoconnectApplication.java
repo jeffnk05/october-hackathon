7fb1d46d889645467758267ebc0df74d94781c41
Jeff <j.edjekoomhene@gmail.com>
2022-10-07 07:35:09 +0200

    init

---
+++ src/main/java/com/jeffnk/advoconnect/AdvoconnectApplication.java
@@ -0,0 +1,13 @@
+package com.jeffnk.advoconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvoconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvoconnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeffnk/advoconnect/AdvoconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeffnk.advoconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvoconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
