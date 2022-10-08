ced910ae5b24a1800e79270a23025691bc173796
Jeff <j.edjekoomhene@gmail.com>
2022-10-08 11:07:06 +0200

    Setting up Postgres connection

---
+++ src/main/java/com/jeffnk/advoconnect/data/Advocate.java
@@ -0,0 +1,13 @@
+package com.jeffnk.advoconnect.data;
+
+
+public class Advocate {
+
+    private long id;
+    private String name;
+    private byte[] profilePicture;
+    private String shortBio;
+    private String longBio;
+    private int yearsOfExperience;
+
+}
---
+++ src/main/java/com/jeffnk/advoconnect/data/Company.java
@@ -0,0 +1,10 @@
+package com.jeffnk.advoconnect.data;
+
+public class Company {
+
+    private long id;
+    private String name;
+    private byte[] logo;
+    private String summary;
+
+}
